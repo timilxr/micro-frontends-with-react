d54bdbfc58db873f39639cf5c49c42b2a35b9505
timilxr <38140090+timilxr@users.noreply.github.com>
2025-10-06 09:30:20 +0100

    dynamic imports of remote components for display

--- packages/container/src/App.jsx
+++ packages/container/src/App.jsx
@@ -2,9 +2,25 @@ import React from "react";
 import "./app.css";
 
 export default function App() {
-  return <HomePage />;
+  return <>
+  <HomePage />
+  <RemoteProducts />
+  <RemoteCart />
+  </>;
 }
 
 function HomePage() {
   return <div>Welcome to our micro-frontend store!</div>;
+}
+
+function RemoteProducts() {
+  const Products = React.lazy(() => import("products/Products"));
+  return (<React.Suspense fallback={<p>Loading products...</p>}>
+    <Products />
+  </React.Suspense>)
+}
+
+function RemoteCart() {
+  const Cart = React.lazy(() => import("cart/Cart"));
+  return (<React.Suspense fallback={<p>loading cart...</p>}><Cart /></React.Suspense>)
 }
