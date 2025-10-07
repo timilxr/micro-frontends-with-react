bddbfb89df988c0fe0813ebdc500031227d23daa
timilxr <38140090+timilxr@users.noreply.github.com>
2025-10-07 11:22:57 +0100

    using context in header

--- packages/container/src/Header.jsx
+++ packages/container/src/Header.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {useCart} from "cart/CartContext";
 
 export default function Header() {
+    const { items } = useCart();
+
   return <header className="header">
     <h1>My Micro-Frontend Store</h1>
     <nav className="nav-links">
       <Link to="/">Home</Link>
       <Link to="/products">Products</Link>
-      <Link to="/cart">Cart</Link>
+      <Link to="/cart">Cart({items.length})</Link>
     </nav>
   </header>;
 }
